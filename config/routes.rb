ac002b3a4a2d5f8c74cd381f07796c59b9bed58e
skipman-77 <h.morikawa5201@gmail.com>
2020-05-30 16:58:05 +0900

    グループ管理とユーザー管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  root "messages#index"
+  devise_for :users
+  root "groups#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update, :index]
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
